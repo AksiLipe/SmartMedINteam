170fbb695559a137b71552d3993ac3f0058fbf30
AksiLipe <fill.123filippov@mail.ru>
2024-11-22 20:20:17 +0300

    added faq demo_site

--- SmartMedINteam/SmartMedINteam/settings.py
+++ SmartMedINteam/SmartMedINteam/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'authorization',
     'SmartMedINteam',
+    'faq',
 ]
 
 MIDDLEWARE = [
--- SmartMedINteam/SmartMedINteam/urls.py
+++ SmartMedINteam/SmartMedINteam/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", include("authorization.urls")),
+    path("faq/", include("faq.urls")),
 ]
